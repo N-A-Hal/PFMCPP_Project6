2069bd56fabde068ddc009d96d2d42995640f5f7
Nikita <119616420+N-A-Hal@users.noreply.github.com>
2024-05-12 05:28:28 +0000

    project6 part1

--- main.cpp
+++ main.cpp
@@ -58,14 +58,18 @@ Purpose:  This project will show you the difference between member functions and
 #include <string>
 struct T
 {
-    T(<#type name#> v, const char* <#variable name#>)   //1
-    //2
-    //3
+    int value;
+    std::string name;
+    T(int v, const char* n)   //1
+    {
+        this->value = v;         //2
+        this->name = n;           //3
+    }
 };
 
-struct <#structName1#>                                //4
+struct L                                //4
 {
-    <#type name#> compare(<#type name#> a, <#type name#> b) //5
+    T* compare(T* a, T* b) //5
     {
         if( a->value < b->value ) return a;
         if( a->value > b->value ) return b;
@@ -73,34 +77,41 @@ struct <#structName1#>                                //4
     }
 };
 
+
 struct U
 {
-    float <#name1#> { 0 }, <#name2#> { 0 };
-    <#returnType#> <#memberFunction#>(<#type name#>* <#updatedValue#>)      //12
+    float m { 0 }, n { 0 };
+    float memberFunction(float* newValue)      //12
     {
-        
+        std::cout << "U's m value: " << this->m << std::endl;
+        this->m = *newValue;
+        std::cout << "U's m updated value: " << this->m << std::endl;
+        while( std::abs(this->n - this->m) > 0.001f )
+        {
+            this->n += 0.0001f * this->m;
+        }
+        std::cout << "U's n updated value: " << this->n << std::endl;
+        return this->n * this->m;
     }
 };
 
-struct <#structname2#>
+struct Q
 {
-    static <#returntype#> <#staticFunctionA#>(U* that, <#type name#>* <#updatedValue#> )        //10
+    static float staticFunction(U* that, float* newValue )        //10
     {
-        std::cout << "U's <#name1#> value: " << that-><#name1#> << std::endl;
-        that-><#name1#> = <#updatedValue#>;
-        std::cout << "U's <#name1#> updated value: " << that-><#name1#> << std::endl;
-        while( std::abs(that-><#name2#> - that-><#name1#>) > 0.001f )
+        std::cout << "U's m value: " << that->m << std::endl;
+        that->m = *newValue;
+        std::cout << "U's m updated value: " << that->m << std::endl;
+        while( std::abs(that->n - that->m) > 0.001f )
         {
-            /*
-             write something that makes the distance between that-><#name2#> and that-><#name1#> get smaller
-             */
-            that-><#name2#> += ;
+            that->n += 0.0001f * that->m;
         }
-        std::cout << "U's <#name2#> updated value: " << that-><#name2#> << std::endl;
-        return that-><#name2#> * that-><#name1#>;
+        std::cout << "U's n updated value: " << that->n << std::endl;
+        return that->n * that->m;
     }
 };
-        
+
+
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
@@ -117,17 +128,33 @@ struct <#structname2#>
 
 int main()
 {
-    T <#name1#>( , );                                             //6
-    T <#name2#>( , );                                             //6
+    T a(35, "John");                                           //6
+    T b(45, "Rob");                                           //6
     
-    <#structName1#> f;                                            //7
-    auto* smaller = f.compare( , );                              //8
-    std::cout << "the smaller one is << " << smaller->name << std::endl; //9
+    T* ptrTo_a = &a;  
+    T* ptrTo_b = &b; 
     
-    U <#name3#>;
+    L f;                                        //7
+
+    if( ptrTo_a != nullptr && ptrTo_b != nullptr )
+    {
+        auto* smaller = f.compare(ptrTo_a, ptrTo_b);                              //8
+        if(smaller != nullptr)
+        {
+            std::cout << "the smaller one is << " << smaller->name << std::endl; //9
+        }
+        else
+        {
+            std::cout << "smaller is nullptr because compared values are equal" << std::endl; 
+        }
+    }
+    
+    
+    U x;
     float updatedValue = 5.f;
-    std::cout << "[static func] <#name3#>'s multiplied values: " << <#structname2#>::<#staticFunctionA#>( , ) << std::endl;                  //11
+    std::cout << "[static func] x's multiplied values: " << Q::staticFunction(&x, &updatedValue) << std::endl;                  //11
     
-    U <#name4#>;
-    std::cout << "[member func] <#name4#>'s multiplied values: " << <#name4#>.<#memberFunction#>( &updatedValue ) << std::endl;
+    U y;
+    std::cout << "[member func] y's multiplied values: " << y.memberFunction(&updatedValue) << std::endl;
+    
 }
