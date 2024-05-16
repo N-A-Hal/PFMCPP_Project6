091a681d1b9f6e61062eba5047089b3f02ef9060
Nikita <119616420+N-A-Hal@users.noreply.github.com>
2024-05-16 23:01:25 +0000

    Part2: converted pointers to references

--- main.cpp
+++ main.cpp
@@ -54,13 +54,11 @@ struct T
 struct L                                //4
 {
 
-    T* compare(T* a, T* b) //5
+    T* compare(T& a, T& b) //5
     {
-        if( a != nullptr && b != nullptr )
-        {
-            if( a->value < b->value ) return a;	
-            if( a->value > b->value ) return b;
-        }
+
+        if( a.value < b.value ) return &a;	
+        if( a.value > b.value ) return &b;
 
         return nullptr;
     } 
@@ -70,15 +68,11 @@ struct L                                //4
 struct U
 {
     float m { 0 }, n { 0 };
-    float memberFunction(float* newValue)      //12
+    float memberFunction(const float& newValue)      //12
     {
-        if(newValue == nullptr)
-        {
-            std::cout << "newValue is nullptr, 0 is returned" << std::endl;
-            return 0.0f;
-        }
+
         std::cout << "U's m value: " << this->m << std::endl;
-        this->m = *newValue;
+        this->m = newValue;
         std::cout << "U's m updated value: " << this->m << std::endl;
         while( std::abs(this->n - this->m) > 0.001f )
         {
@@ -91,55 +85,34 @@ struct U
 
 struct Q
 {
-    static float staticFunction(U* that, float* newValue )        //10
+    static float staticFunction(U& that, const float& newValue )        //10
     {
-        if(that == nullptr)
-        {
-            std::cout << "that is nullptr, 0 is returned" << std::endl;
-            return 0.0f;
-        }
-        if(newValue == nullptr)
-        {
-            std::cout << "newValue is nullptr, 0 is returned" << std::endl;
-            return 0.0f;
-        }
         
-        std::cout << "U's m value: " << that->m << std::endl;
-        that->m = *newValue;
-        std::cout << "U's m updated value: " << that->m << std::endl;
-        while( std::abs(that->n - that->m) > 0.001f )
+        std::cout << "U's m value: " << that.m << std::endl;
+        that.m = newValue;
+        std::cout << "U's m updated value: " << that.m << std::endl;
+        while( std::abs(that.n - that.m) > 0.001f )
         {
-            that->n += 0.0001f * that->m;
+            that.n += 0.0001f * that.m;
         }
-        std::cout << "U's n updated value: " << that->n << std::endl;
-        return that->n * that->m;
+        std::cout << "U's n updated value: " << that.n << std::endl;
+        return that.n * that.m;
     }
 };
 
 
 int main()
 {
-    T a(35, "John");                                           //6
-    T b(35, "Rob");                                           //6
-    
-    T* ptrTo_a = &a;  
-    T* ptrTo_b = &b; 
+    T a(55, "John");                                           //6
+    T b(45, "Rob");                                           //6
     
     L f;                                        //7
 
    
-    auto* smaller = f.compare(ptrTo_a, ptrTo_b);                              //8
+    auto* smaller = f.compare(a, b);                              //8
     if(smaller != nullptr)
     {
         std::cout << "the smaller one is << " << smaller->name << std::endl; //9
-    }
-    else if (ptrTo_a == nullptr)
-    {
-        std::cout << "ptrTo_a is nullptr" << std::endl;
-    }
-    else if (ptrTo_b == nullptr)
-    {
-        std::cout << "ptrTo_b is nullptr" << std::endl;
     }
     else
     {
@@ -149,9 +122,9 @@ int main()
     
     U x;
     float updatedValue = 5.f;
-    std::cout << "[static func] x's multiplied values: " << Q::staticFunction(&x, &updatedValue) << std::endl;                  //11
+    std::cout << "[static func] x's multiplied values: " << Q::staticFunction(x, updatedValue) << std::endl;                  //11
     
     U y;
-    std::cout << "[member func] y's multiplied values: " << y.memberFunction(&updatedValue) << std::endl;
+    std::cout << "[member func] y's multiplied values: " << y.memberFunction(updatedValue) << std::endl;
     
 }
